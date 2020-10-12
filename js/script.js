83e92e8c0eb0024c018bcfc66e9f118cb5475ccc
nardo88 <nardo1988@mail.ru>
2020-10-12 15:44:09 +0300

    main section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
+
+
 const burgerButton = document.querySelector('.burger-button'),
-    nav = document.querySelector('.header__nav-links');
+    nav = document.querySelector('.header__nav-links'),
+    tabContent = document.querySelectorAll('.topic-article'),
+    tabItem = document.querySelectorAll('.recommendation__item');
+
+
+// бургер меню
 
 burgerButton.addEventListener('click', () => {
     nav.classList.toggle('menu-open')
+})
+
+// табы
+
+tabItem.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        for (let i = 0; i < tabItem.length; i++){
+            tabContent[i].classList.remove('active-tab')
+            tabItem[i].classList.remove('active-item')
+        }
+        tabContent[index].classList.add('active-tab')
+        tabItem[index].classList.add('active-item')
+    })
 })
