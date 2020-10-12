f40cdf6cdda133d3de4b646c49702949dfbcaf5c
nardo88 <nardo1988@mail.ru>
2020-10-12 18:05:44 +0300

    new

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 const burgerButton = document.querySelector('.burger-button'),
     nav = document.querySelector('.header__nav-links'),
     tabContent = document.querySelectorAll('.topic-article'),
-    tabItem = document.querySelectorAll('.recommendation__item');
+    tabItem = document.querySelectorAll('.recommendation__item'),
+    recommendationTitle = document.querySelector('.recommendation__title'),
+    recommendationList = document.querySelector('.recommendation__list');
 
 
 // бургер меню
@@ -22,5 +24,15 @@ tabItem.forEach((item, index) => {
         }
         tabContent[index].classList.add('active-tab')
         tabItem[index].classList.add('active-item')
+
+        if (window.innerWidth < 680){
+            recommendationList.classList.toggle('recommendation__list--open')
+        }
+
     })
+})
+
+recommendationTitle.addEventListener('click', () => {
+    console.log(1);
+    recommendationList.classList.toggle('recommendation__list--open')
 })
